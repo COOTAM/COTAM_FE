a22b0ef477d0ee69bb07b026502618a956928d9c
jangseyeong <jang.se.yeong000@gmail.com>
2024-09-29 01:03:30 +0900

    feat: 헤더 반응형 추가

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -4,12 +4,12 @@ import { ROUTES } from '@/constants/routes';
 
 const Footer = () => {
   return (
-    <footer className="w-full px-10 pb-10 pt-[60px] flex-center bg-pixel-gradient">
-      <div className="flex w-full flex-col justify-center gap-[60px] lg:w-[1024px]">
+    <footer className="w-full flex-center bg-pixel-gradient">
+      <div className="flex w-full flex-col justify-center gap-[60px] px-10 pb-10 pt-[60px] lg:w-[1024px]">
         <Cotam className="h-[18px] w-20 text-white" />
         <ul className="flex flex-col justify-center gap-4 text-white galmuri11-body-2">
           {Object.values(ROUTES).map((ROUTE) => (
-            <li key={ROUTE.LABEL}>
+            <li key={ROUTE.PATH}>
               <Link href={ROUTE.PATH}>{ROUTE.LABEL}</Link>
             </li>
           ))}
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,6 +1,6 @@
 export const ROUTES = {
   HOME: { LABEL: '홈', PATH: '/' },
-  PEOPLE: { LABEL: '사람들', PATH: '/people' },
   ABOUT: { LABEL: '코탐 소개', PATH: '/about' },
+  PEOPLE: { LABEL: '사람들', PATH: '/people' },
   RECRUIT: { LABEL: '모집 안내', PATH: '/recruit' },
 } as const;
