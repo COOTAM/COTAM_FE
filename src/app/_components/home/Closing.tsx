96cc2b302e2c653dd4636d48ce901cfab7e24287
jangseyeong <jang.se.yeong000@gmail.com>
2024-10-16 16:25:49 +0900

    feat: 현재 모집 여부 설정값 추가

--- src/app/_components/home/Closing.tsx
+++ src/app/_components/home/Closing.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import HandMetal from '../../../../public/assets/icons/HandMetalRed.svg';
 import WithCotamButton from '../WithCotamButton';
 
--- src/app/recruit/page.tsx
+++ src/app/recruit/page.tsx
@@ -18,25 +18,31 @@ export default function Recruit() {
     <main className="box-border flex min-h-screen w-full flex-col gap-y-[100px] p-10 lg:w-[1024px]">
       <section aria-label="모집 일정" className="flex flex-col gap-y-6">
         <h2 className="text-white galmuri11-headline-3">모집 일정</h2>
-        <div className="flex flex-col gap-y-6">
-          <div className="flex flex-col gap-3 md:flex-row">
-            <DescriptionCard
-              variant="lightBlue"
-              title="모집 기간"
-              contents={recruitConfig.recruitmentPeriod}
-              className="flex-1"
-            />
-            <DescriptionCard
-              variant="lightBlue"
-              title="발표"
-              contents={recruitConfig.announcementDate}
-              className="flex-1"
-            />
+        {recruitConfig.isRecruitmentOpen ? (
+          <div className="flex flex-col gap-y-6">
+            <div className="flex flex-col gap-3 md:flex-row">
+              <DescriptionCard
+                variant="lightBlue"
+                title="모집 기간"
+                contents={recruitConfig.recruitmentPeriod}
+                className="flex-1"
+              />
+              <DescriptionCard
+                variant="lightBlue"
+                title="발표"
+                contents={recruitConfig.announcementDate}
+                className="flex-1"
+              />
+            </div>
+            <a href={RECRUITMENT_FORM_URL} target="_blank" rel="noopener noreferrer">
+              <Button className="w-full">코탐 지원하기 ⇗</Button>
+            </a>
           </div>
-          <a href={RECRUITMENT_FORM_URL} target="_blank" rel="noopener noreferrer">
-            <Button className="w-full">코탐 지원하기 ⇗</Button>
-          </a>
-        </div>
+        ) : (
+          <div className="rounded-xl bg-cotam-blue-90 px-[30px] py-[35px] text-white galmuri11-body-1 flex-center">
+            지금은 모집 기간이 아니에요
+          </div>
+        )}
       </section>
       <section aria-label="지원 자격" className="flex flex-col gap-y-6">
         <h2 className="text-white galmuri11-headline-3">지원 자격</h2>
