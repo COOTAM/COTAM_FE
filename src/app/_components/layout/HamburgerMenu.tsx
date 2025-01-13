98d770067786dd5e6e710c4bc100a72bc4001750
JANGSEYEONG <jang.se.yeong000@gmail.com>
2025-01-13 15:05:02 +0900

    refactor: Drawer 구조 변경 및 메뉴 UI 요구사항에 맞게 수정
    shadcn/ui의 Drawer 참고하여 구조 변경

--- src/app/_components/layout/HamburgerMenu.tsx
+++ src/app/_components/layout/HamburgerMenu.tsx
@@ -1,22 +1,39 @@
-'use client';
+import Link from 'next/link';
 
-import { useReducer } from 'react';
+import Drawer from '@/components/Drawer';
+
+import { ROUTES } from '@/constants/routes';
 
 import Hamburger from '../../../../public/assets/icons/Hamburger.svg';
-import MenuDrawer from '../../../components/MenuDrawer';
+import MenuItem from './MenuItem';
 
-interface MenuButtonProps extends React.HTMLAttributes<HTMLDivElement> {
+interface Props {
   pathname: string;
   className?: string;
 }
 
-const HamburgerMenu = ({ pathname, className, ...props }: MenuButtonProps) => {
-  const [isOpen, toggleIsOpen] = useReducer((prev) => !prev, false);
-
+const HamburgerMenu = ({ pathname, className }: Props) => {
   return (
-    <div className={className} {...props}>
-      <Hamburger className="h-6 w-6 cursor-pointer" onClick={toggleIsOpen} />
-      <MenuDrawer isOpen={isOpen} onClose={toggleIsOpen} pathname={pathname} />
+    <div className={className}>
+      <Drawer>
+        <Drawer.Trigger>
+          <Hamburger className="h-6 w-6 cursor-pointer" />
+        </Drawer.Trigger>
+        <Drawer.Content className="bg-[#08072D]/50 backdrop-blur-[50px]">
+          <ul className="mt-4">
+            {Object.values(ROUTES).map((ROUTE) => (
+              <li key={ROUTE.PATH}>
+                <Drawer.Close>
+                  <Link href={ROUTE.PATH}>
+                    <MenuItem text={ROUTE.LABEL} selected={pathname === ROUTE.PATH} />
+                  </Link>
+                </Drawer.Close>
+              </li>
+            ))}
+          </ul>
+        </Drawer.Content>
+      </Drawer>
+      {/* <Drawer isOpen={isOpen} onClose={toggleIsOpen} pathname={pathname} /> */}
     </div>
   );
 };
